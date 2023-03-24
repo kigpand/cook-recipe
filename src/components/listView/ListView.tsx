39f5c32974a2b94fe64b9067464a49f9de292e72
김지훈 <kigpand@gmail.com>
2023-03-24 22:22:50 +0900

    list view ui 작업

--- src/components/listView/ListView.tsx
+++ src/components/listView/ListView.tsx
@@ -5,7 +5,33 @@ const ListView = () => {
   const { currentRecipe } = useRecipe();
   return (
     <div className={styles.listView} id="listView">
-      {currentRecipe?.name}
+      <div className={styles.title}>{currentRecipe?.name}</div>
+      <img src={currentRecipe!.imgUrl} alt="img" className={styles.img} />
+      <div className={styles.materials}>
+        <div className={styles.materialTitle}>재료</div>
+        <div className={styles.items}>
+          {currentRecipe!.material.map((item: string, i: number) => {
+            return (
+              <div className={styles.item} key={i}>
+                {item}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div className={styles.recipe}>
+        <div className={styles.recipeTitle}>조리 방법</div>
+        <div className={styles.recipeList}>
+          {currentRecipe!.recipe.map((item: string, i: number) => {
+            return (
+              <div className={styles.list} key={i}>
+                <b>{i + 1}.</b>
+                {item}
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 };
