9620c6aefa1964d27bae1382359b33f9519a25b2
김지훈 <kigpand@gmail.com>
2023-04-07 18:26:32 +0900

    mobile 새로고침 버튼 추가

--- src/components/header/mobile/MobileHeader.tsx
+++ src/components/header/mobile/MobileHeader.tsx
@@ -1,9 +1,11 @@
 import { useCallback, useState } from "react";
 import styles from "./MobileHeader.module.scss";
 import ToggleHeader from "./toggleHeader/ToggleHeader";
+import useRecipe from "../../../store/recipe";
 
 const MobileHeader = () => {
   const [toggle, setToggle] = useState<boolean>(false);
+  const { recipes, saveRecipes, setRecipes } = useRecipe();
 
   const onCloseToggle = useCallback(() => {
     setToggle(false);
@@ -11,6 +13,14 @@ const MobileHeader = () => {
 
   return (
     <div className={styles.mobileHeader}>
+      {recipes.length !== saveRecipes.length && (
+        <img
+          src={`${process.env.PUBLIC_URL}/imgs/reload.png`}
+          alt="reload"
+          className={styles.reload}
+          onClick={() => setRecipes(saveRecipes)}
+        />
+      )}
       <img
         src={`${process.env.PUBLIC_URL}/imgs/toggle.png`}
         alt="toggle"
